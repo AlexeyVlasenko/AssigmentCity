5b7d8a1517e5feb378eb713f3b4874373fcf6844
Alexey Vlasenko <inetgameslam@gmail.com>
2019-04-07 16:54:50 +0300

    added missed dependencies

--- App.js
+++ App.js
@@ -1,71 +1,16 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Dimensions,
-  ScrollView,
-} from 'react-native';
-import MapView, { Marker, ProviderPropType } from 'react-native-maps';
+import React, { Component } from 'react';
+import { View } from "react-native";
 
-const { width, height } = Dimensions.get('window');
 
-const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+class App extends Component {
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+    render(){
+        return (
+            <View>
 
-    this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-    };
-  }
-
-  render() {
-    return (
-        <View style={styles.container}>
-          <MapView
-              provider={this.props.provider}
-              style={styles.map}
-              initialRegion={this.state.region}
-          >
-            <Marker
-                title="This is a title"
-                description="This is a description"
-                coordinate={this.state.region}
-            />
-          </MapView>
-        </View>
-    );
-  }
+            </View>
+        )
+    }
 }
 
-App.propTypes = {
-  provider: ProviderPropType,
-};
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  scrollview: {
-    alignItems: 'center',
-    paddingVertical: 40,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
-
-export default App;
+export default App
