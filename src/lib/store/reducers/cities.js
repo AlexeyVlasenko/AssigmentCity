b57d84ac4a777a19fa170c0f4f735e53b28eb263
Alexandr Dobrovolskiy <sasha732sasha000@gmail.com>
2019-04-08 12:17:58 +0300

    api mock, cities store

---
+++ src/lib/store/reducers/cities.js
@@ -0,0 +1,13 @@
+import { cities as ActionTypes } from '../actionTypes';
+
+const initialState = {
+    list: [],
+    search: [],
+};
+
+export default (state = initialState, action) => {
+    const { type } = action;
+    switch (type) {
+
+    }
+};
