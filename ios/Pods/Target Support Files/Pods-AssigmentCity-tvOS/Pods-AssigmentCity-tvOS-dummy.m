e2bcb1e3a47600380ff9c0bea03b7167c27a0873
Alexey Vlasenko <inetgameslam@gmail.com>
2019-04-08 21:05:10 +0300

    fixed pods bug

--- ios/Pods/Target Support Files/Pods-AssigmentCity-tvOS/Pods-AssigmentCity-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AssigmentCity_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_AssigmentCity_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-AssigmentCityTests/Pods-AssigmentCityTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AssigmentCityTests : NSObject
-@end
-@implementation PodsDummy_Pods_AssigmentCityTests
-@end
