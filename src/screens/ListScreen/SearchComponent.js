291967b5b0cc5df46819d9b86e95cc016b394208
Alexandr Dobrovolskiy <sasha732sasha000@gmail.com>
2019-04-09 15:53:57 +0300

    list scree refresh impl, minor refactor, weather

--- src/screens/ListScreen/SearchComponent.js
+++ src/screens/ListScreen/SearchComponent.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Platform, SafeAreaView, StyleSheet, View } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { Theme } from "#theme";
 
-// @TODO: Move to functional component
-class SearchComponent extends Component {
-    render() {
-        const { onChangeText, value, onIconPress } = this.props;
-
-        return (
-            <SafeAreaView style={{ backgroundColor: Theme.HEADER_BG }}>
-                <View style={styles.container}>
-                    <Searchbar
-                        style={styles.searchBar}
-                        placeholder="Search Cities..."
-                        onChangeText={onChangeText}
-                        value={value}
-                        onIconPress={onIconPress}
-                    />
-                </View>
-            </SafeAreaView>
-        );
-    }
-}
+const SearchComponent = ({ onChangeText, value, onIconPress }) => (
+    <SafeAreaView style={{ backgroundColor: Theme.HEADER_BG }}>
+        <View style={styles.container}>
+            <Searchbar
+                style={styles.searchBar}
+                placeholder="Search Cities..."
+                onChangeText={onChangeText}
+                value={value}
+                onIconPress={onIconPress}
+            />
+        </View>
+    </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
     container: {
